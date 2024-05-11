3a24fc8c94d6637e63a47589f82507d666994c82
agata <e12023151@student.tuwien.ac.at>
2024-05-11 16:24:03 +0200

    added angular material creation of user works in frontend and backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {UsersComponent} from "./users/users.component";
+import {UserService} from "./user.service";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UsersComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
+  constructor(private userService: UserService,
+              private httpClient: HttpClient
+  ) {
+  }
+
   title = 'frontend';
 }
