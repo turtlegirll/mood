1b867f6cd92885158d4a10604e0aafa6ae897550
agata <e12023151@student.tuwien.ac.at>
2024-05-13 17:36:05 +0200

    added graphs and card to front page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,25 @@ import {RouterOutlet} from '@angular/router';
 import {UsersComponent} from "./users/users.component";
 import {UserService} from "./user.service";
 import {HttpClient} from "@angular/common/http";
+import {MatIcon} from "@angular/material/icon";
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatButton, MatIconButton} from "@angular/material/button";
+import {
+  MatCard,
+  MatCardActions,
+  MatCardContent,
+  MatCardHeader,
+  MatCardImage, MatCardSubtitle,
+  MatCardTitle
+} from "@angular/material/card";
+import {MatExpansionPanelTitle} from "@angular/material/expansion";
+import {NgIf} from "@angular/common";
+import {MoodCardComponent} from "./mood-card/mood-card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UsersComponent],
+  imports: [RouterOutlet, UsersComponent, MatIcon, MatToolbar, MatIconButton, MatCard, MatCardHeader, MatCardContent, MatCardActions, MatButton, MatCardImage, MatCardTitle, MatCardSubtitle, MatExpansionPanelTitle, NgIf, MoodCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
