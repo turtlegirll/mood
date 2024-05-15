e1bec083f6d7a91b1acdf590b0d5d87041363c66
agata <e12023151@student.tuwien.ac.at>
2024-05-15 13:31:38 +0200

    small fixes + create mood impl

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router';
 import {CreateMoodComponent} from "./create-mood/create-mood.component";
-import {UsersComponent} from "./users/users.component";
 
 import {HomepageComponent} from "./homepage/homepage.component";
 
 export const routes: Routes = [
   { path: '', component: HomepageComponent },
-  { path: 'log-mood', component: UsersComponent },
-  { path: 'second-component', component: CreateMoodComponent },
+  { path: 'log-mood', component: CreateMoodComponent },
 ];
--- src/app/create-mood/create-mood.component.ts
+++ src/app/create-mood/create-mood.component.ts
@@ -1,12 +1,58 @@
 import { Component } from '@angular/core';
+import {MatGridList, MatGridTile, MatGridTileText} from "@angular/material/grid-list";
+import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {format} from "date-fns";
+import {
+  MatAccordion,
+  MatExpansionPanel,
+  MatExpansionPanelDescription, MatExpansionPanelHeader,
+  MatExpansionPanelTitle
+} from "@angular/material/expansion";
+import {MatSlider, MatSliderThumb} from "@angular/material/slider";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-create-mood',
   standalone: true,
-  imports: [],
+  imports: [
+    MatGridList,
+    MatGridTile,
+    MatGridTileText,
+    MatCard,
+    MatCardTitle,
+    MatCardContent,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    MatCardHeader,
+    MatExpansionPanelDescription,
+    MatExpansionPanelTitle,
+    MatExpansionPanel,
+    MatAccordion,
+    MatExpansionPanelHeader,
+    MatSlider,
+    MatSliderThumb,
+    FormsModule
+  ],
   templateUrl: './create-mood.component.html',
   styleUrl: './create-mood.component.scss'
 })
 export class CreateMoodComponent {
+  panelOpenState = false;
+  today: Date = new Date();
+  disabled = false;
+  max = 12;
+  min = 0;
+  showTicks = false;
+  step = 0.5;
+  thumbLabel = false;
+  hours_slept = 0;
+  formattedDate: string;
 
+  constructor() {
+    // Format the date as "dd.MM.yyyy"
+    this.formattedDate = format(this.today, 'dd.MM.yyyy');
+  }
 }
